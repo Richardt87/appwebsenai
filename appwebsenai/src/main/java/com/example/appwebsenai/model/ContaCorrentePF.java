3ed9a7423acb9b74facc42556e2550545ca406bb
Richardt87 <105252260+Richardt87@users.noreply.github.com>
2023-08-17 22:18:44 -0300

    #Correção e modificações

--- appwebsenai/src/main/java/com/example/appwebsenai/model/ContaCorrentePF.java
+++ appwebsenai/src/main/java/com/example/appwebsenai/model/ContaCorrentePF.java
@@ -1,5 +1,11 @@
 package com.example.appwebsenai.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ContaCorrentePF extends Conta {
+    @javax.persistence.Id
+    private  Long Id;
 
 }
