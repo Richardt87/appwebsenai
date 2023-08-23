bdb4ce94257cc112da6fa02d920e6bf213f3879e
Richardt87 <105252260+Richardt87@users.noreply.github.com>
2023-08-23 18:05:24 -0300

    #Criação script.js #style.css #index.html

---
+++ appwebsenai/src/main/resources/static/script.js
@@ -0,0 +1,8 @@
+const formPessoa = document.getElementById("cadastroPessoas")
+
+formPessoa.addEventListener("submit", function (event){
+   event.preventDefault();
+   alert("Hello World");
+
+
+});
