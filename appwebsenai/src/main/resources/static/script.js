19e1e5e9a850abfd39d4d6d3402ce612ed107d4c
Richardt87 <105252260+Richardt87@users.noreply.github.com>
2023-08-24 00:36:53 -0300

    #Adicionado metodo adicionar pessoa

--- appwebsenai/src/main/resources/static/script.js
+++ appwebsenai/src/main/resources/static/script.js
@@ -1,8 +1,20 @@
-const formPessoa = document.getElementById("cadastroPessoas")
+const formPessoa = document.getElementById("cadastroPessoa")
 
 formPessoa.addEventListener("submit", function (event){
    event.preventDefault();
-   alert("Hello World");
+   // let nome = document.getElementById("nome").value;
+   // document.getElementById("sexo").value = nome;
+
+    let formDados = new FormData(formPessoa);
+    let parametros = new URLSearchParams(formDados);
+
+
+   fetch("/person?"+parametros.toString(), {
+      method: "POST"
+   }).then(response => response.json())
+       .then(data => {
+          document.getElementById("id").value = data.id;
+       })
 
 
 });
