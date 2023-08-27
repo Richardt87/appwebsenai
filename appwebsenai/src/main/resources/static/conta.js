ac1679d091680af043b89a105b45ec875dbde948
Richardt87 <105252260+Richardt87@users.noreply.github.com>
2023-08-26 22:28:49 -0300

    #

--- appwebsenai/src/main/resources/static/conta.js
+++ appwebsenai/src/main/resources/static/conta.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", function (){
         fetch("/all")
             .then(response => response.getJSON())
             .then(data => {
-                let option = document.createElement("option");
+                /*let option = document.createElement("option");
                 option.value = null;
                 option.textContent = pessoa.name;
-                comboPessoa.appendChild(option);
-                /*data.forEach(pessoa => {
+                comboPessoa.appendChild(option);*/
+                data.forEach(pessoa => {
                     let option =document.createElement("option");
                     option.value = pessoa.id;
                     option.textContent = pessoa.name;
                     comboPessoa.appendChild(option);
 
-                })*/
+                })
             })
 
     }
